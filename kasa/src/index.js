11aff9c125a57845ddaae4feff7167ac8e251a99
incisif <loeildejack@gmail.com>
2023-05-22 03:28:06 +0200

    Set up project folder structure

---
+++ kasa/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Header from './components/Header'
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <React.StrictMode>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+      <Home />
+    </Router>
+  </React.StrictMode>
+)
---
+++ kasa/src/pages/Home/index.jsx
@@ -0,0 +1,13 @@
+
+
+function Home() {
+ 
+  return (
+    <div className="Home">
+      
+    </div>
+    
+  );
+}
+
+export default Home;
